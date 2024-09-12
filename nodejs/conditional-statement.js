8908fbc5dafdaeb26ea981ab64fbff309811f55c
jamesjoel <james.joelj@gmail.com>
2024-09-12 19:21:16 +0530

    loop in js

---
+++ nodejs/conditional-statement.js
@@ -0,0 +1,20 @@
+// if(conditon){
+
+// code
+
+// }
+
+// let a = 20;
+
+// if(a % 2 == 0 && a % 5 == 0)
+// {
+//     console.log("YES")
+// }
+// 1980
+
+let a = 2028;
+if(a % 12 == 0)
+{
+    console.log("YES")
+}
+
---
+++ nodejs/switch.js
@@ -0,0 +1,22 @@
+let a = "B";
+
+switch(a)
+{
+    case "A" : console.log("indore");
+            break;
+    
+    case "B" : console.log("mumbai");
+            break;
+    
+    case "C" : console.log("pune");
+            break;
+    
+    case "D" : console.log("delhi");
+            break;
+    
+    case "E" : console.log("bhopal");
+            break;
+
+    default : console.log("no city found")
+
+}
